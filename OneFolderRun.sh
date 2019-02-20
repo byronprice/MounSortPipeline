68ce2c568718ee892d1b1f3ac696b5644732662f
Byron Price <byron.h.price@gmail.com>
2019-02-20 17:29:06 -0500

    add file to run only one ephys folder

---
+++ OneFolderRun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source activate mountainsort
+
+cp ~/Documents/Current-Projects/MounSortPipeline/params.json .
+counter=0
+for i in $( ls raw.nt* ); do
+	let counter=counter+1
+	ml-run-process ephys.whiten --inputs timeseries:$i --outputs timeseries_out:pre.mda$counter.prv
+	ml-run-process ms4alg.sort --inputs timeseries:pre.mda$counter.prv --outputs firings_out:firings$counter.mda --parameters detect_sign:-1 adjacency_radius:-1 detect_threshold:3 clip_size:40
+done
+
+source deactivate mountainsort
